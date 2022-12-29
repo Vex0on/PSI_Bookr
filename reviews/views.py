712a9ee6ec30bae9b1bb20bbcf72a115cbce759e
Vex0on <Iplayagame101@gmail.com>
2022-12-29 20:32:07 +0100

    Add basic views, templates and urls

--- reviews/views.py
+++ reviews/views.py
@@ -1,9 +1,11 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    name = request.GET.get("name") or "świecie"
-    return HttpResponse("Witaj, {}!".format(name))
+    name = "świecie"
+    return render(request, "base.html", {"name": name})
 
 
-
+def book_search(request):
+    search_text = request.GET.get("search", "")
+    return render(request, "search-results.html", {"search_text": search_text})
