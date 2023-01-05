a2ea2c1cfa36bb39e301606ee3a0ffd53392aa6c
Vex0on <Iplayagame101@gmail.com>
2023-01-05 13:45:14 +0100

    URL mapping, views and templates

--- bookrApp/reviews/urls.py
+++ bookrApp/reviews/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.welcome_view, name="welcome_view"),
     path('book-search/', views.book_search, name='book-search'),
     path('books/', views.book_list, name='book_list'),
+    path('books/<int:pk>/', views.book_detail, name='book_detail'),
 ]
